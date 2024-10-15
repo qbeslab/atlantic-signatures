qbeslab/atlantic-signatures

commit 9e14b8f11b0d90f56e3bb80d7a4e954b53239ec7

Delay imports so that CLI can run when matplotlib is missing

atlantic_signatures/__main__.py
@@ -10,24 +10,23 @@ from pathlib import Path
 import tempfile
 import re
 
-from atlantic_signatures.client import Client
-from atlantic_signatures.host import Host
-from atlantic_signatures.plotter.plot import AnimatedPlot
-
 
 # assume we are running as host if the OS is Windows
 RUNNING_AS_HOST = os.environ.get('RUNNING_AS_HOST', sys.platform == 'win32')
 
 
 def host_run(args):
+    from atlantic_signatures.host import Host
     return Host(config_file=args.config_file, objectname=args.objectname, host=args.host, timeout=args.timeout)
 
 
 def client_run(args):
+    from atlantic_signatures.client import Client
     return Client(host=args.host)
 
 
 def plot_animated(args):
+    from atlantic_signatures.plotter.plot import AnimatedPlot
     for file in args.file:
         file = Path(file)
         print(f'Reading "{file}"')

commit 48d8dde39b3f5cbdd49308eec8344441febf94d6

Fix bug introduced in 9e14b8f
`Host`, at least, needs to be imported sooner

atlantic_signatures/__main__.py
@@ -10,18 +10,19 @@ from pathlib import Path
 import tempfile
 import re
 
+from atlantic_signatures.client import Client
+from atlantic_signatures.host import Host
+
 
 # assume we are running as host if the OS is Windows
 RUNNING_AS_HOST = os.environ.get('RUNNING_AS_HOST', sys.platform == 'win32')
 
 
 def host_run(args):
-    from atlantic_signatures.host import Host
     return Host(config_file=args.config_file, objectname=args.objectname, host=args.host, timeout=args.timeout)
 
 
 def client_run(args):
-    from atlantic_signatures.client import Client
     return Client(host=args.host)
 
 